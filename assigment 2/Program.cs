2abf759c121d1035bedf57bb3cfe08542bbdfbb7
Pham Thanh Dat <ciculscute221@gmail.com>
2026-01-30 15:17:35 +0700

    CPL26/DatPT: Add Multiply function

--- assigment 2/Program.cs
+++ assigment 2/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+private Double calculate(SIGN sign, Double arg1, Double arg2)
+{
+    Double result = 0;
+    switch (sign)
+    {
+        case MULTIPLY:
+            result = arg1 * arg2;
+            break;
+    }
+    return result;
+}
