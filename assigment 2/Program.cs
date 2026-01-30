57d55521cd4a1a8bd9fd45ffa9937088357d07d6
Pham Thanh Dat <ciculscute221@gmail.com>
2026-01-30 15:17:35 +0700

    CPL26/DatPT: Add Multiply function

--- assigment 2/Program.cs
+++ assigment 2/Program.cs
@@ -12,6 +12,8 @@ private Double calculate(SIGN sign, Double arg1, Double arg2)
             result = arg1 - arg2;
         case DIVIDE:
             result = arg1 / arg2;
+        case MULTIPLY:
+            result = arg1 * arg2;
             break;
     }
     return result;
