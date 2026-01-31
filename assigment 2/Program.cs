9047d21816c84b00a93a1f44e9312a66c4d3f3a4
JimmyCiculs <96069122+Ciculs@users.noreply.github.com>
2026-01-31 19:17:00 +0700

    Revert "Feature/cpl26/dat pt/implement multiply: Add divide, multiply, plus, minus function"

--- assigment 2/Program.cs
+++ assigment 2/Program.cs
@@ -1,20 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-private Double calculate(SIGN sign, Double arg1, Double arg2)
-{
-    Double result = 0;
-    switch (sign)
-    {
-        case PLUS:
-            result = arg1 + arg2;
-        case MINUS:
-            result = arg1 - arg2;
-        case DIVIDE:
-            result = arg1 / arg2;
-        case MULTIPLY:
-            result = arg1 * arg2;
-            break;
-    }
-    return result;
-}
